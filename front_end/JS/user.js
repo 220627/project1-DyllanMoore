f2406e63921cd9674b84c689310baad1d363443a
DyllanMoore <dyllantmoore@gmail.com>
2022-07-19 17:11:05 -0500

    created hidden container consts

--- front_end/JS/user.js
+++ front_end/JS/user.js
@@ -10,3 +10,20 @@ const userUpdateUsernameButton = document.getElementById("user-update-username-b
 const userUpdatePasswordButton = document.getElementById("user-update-password-button");
 const userUpdateEmailButton = document.getElementById("user-update-email-button");
 const userUpdateRoleButton = document.getElementById("user-update-role-button");
+
+//Hidden Div Containers
+const userReimbursementOptionsContainer = document.getElementById("user-reimbursement-options-container");
+const createUserReimbursementContainer = document.getElementById("create-user-reimbursement-container");
+const userViewAllReimbursementsContainer = documnet.getElementById("user-view-all-reimbursements-container");
+const userViewPendingReimbursementsContainer = document.getElementById("user-view-pending-reimbursements-container");
+const userUpdateChoiceContainer = document.getElementById("user-update-choice-container");
+const userUpdateNameChoiceContainer = document.getElementById("user-update-name-choice-container")
+const userUpdateFirstNameContainer = document.getElementById("user-update-first-name-container");
+const userUpdateLastNameContainer = document.getElementById("user-update-last-name-container");
+const userUpdateBothNamesContainer = document.getElementById("user-update-both-names-container");
+const userUpdateUsernameContaienr = document.getElementById("user-update-username-container");
+const userUpdatePasswordContainer = document.getElementById("user-update-password-container");
+const userUpdateEmailContainer = document.getElementById("user-update-email-container");
+const userUpdateRoleContainer = document.getElementById("user-update-role-container");
+
+//Toggle Forms Hidden Containers
